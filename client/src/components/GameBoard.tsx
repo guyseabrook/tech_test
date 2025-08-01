8b7366d3d810ab1706b33b296cdbcd5ef151cd30
guyseabrook <guy.e.seabrook@gmail.com>
2025-08-01 16:19:09 +0100

    v3.1 fix overspill of buttons

--- client/src/components/GameBoard.tsx
+++ client/src/components/GameBoard.tsx
@@ -3,9 +3,11 @@ import { XorO } from "../types";
 
 export default function GameBoard({
   board,
+  boardSize = 3,
   onSelectSquare,
 }: {
   board: (XorO | undefined)[][];
+  boardSize: number;
   onSelectSquare: ({
     rowIndex,
     colIndex,
@@ -15,12 +17,18 @@ export default function GameBoard({
   }) => void;
 }) {
   return (
-    <div className="grid grid-cols-3 gap-2">
+    <div
+      className={"max-w-screen-xlg grid justify-center gap-2 overflow-auto p-4"}
+      style={{
+        gridTemplateColumns: `repeat(${boardSize}, minmax(0, 1fr))`,
+        gridTemplateRows: `repeat(${boardSize}, minmax(0, 1fr))`,
+      }}
+    >
       {board.map((row, rowIndex) =>
         row.map((cell, colIndex) => (
           <button
             key={`${rowIndex}-${colIndex}`}
-            className="h-40 w-40 items-center justify-center rounded-md border border-gray-300 text-7xl text-white hover:border-gray-50"
+            className="aspect-square w-full min-w-20 items-center justify-center rounded-md border border-gray-300 text-7xl text-white hover:border-gray-50"
             onClick={() => onSelectSquare({ rowIndex, colIndex })}
             disabled={!!cell}
           >
---
+++ client/src/components/GreenButton.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function GreenButton({ onClick, children, ...props }) {
+  return (
+    <button
+      className="min-w-10 rounded-md bg-spruceDarkGreen px-4 py-2 text-white transition-colors duration-200 hover:bg-spruceGreen"
+      onClick={onClick}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
